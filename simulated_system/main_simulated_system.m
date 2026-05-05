30c532b3139d9c595d73a0cdc1c87c98a13f5900
lucafalcone <luca.falconew@gmail.com>
2026-05-05 21:03:48 +0200

    Refactor simulation; add simulate_system helper
    Reorganize simulation assets and refactor main simulation flow. Moves LQR-related files into simulated_system/LQR, removes top-level main.m, and deletes an old LQR result. Replaces the inline sim/run/plot/save sequence in simulated_system/main_simulated_system.m with a call to a new simulate_system(h,Tsim,x0,u,param,save_results,view_traj) helper that uses Simulink.SimulationInput and supports optional saving and trajectory visualization. Adds example system-identification script (sysIDsim.m) and several new binary sim result files under simulated_system/sim_results.

--- simulated_system/main_simulated_system.m
+++ simulated_system/main_simulated_system.m
@@ -6,7 +6,7 @@ Tsim = 10;
 x0 = [0 0 0 0]';
 
 t_u = (0:h:Tsim)';
-u = timeseries(1*sin(2*pi*t_u), t_u);
+u = timeseries(1*sin(1*pi*t_u), t_u);
 
 param.M   = 0.2;     % cart mass [kg]
 param.m   = 0.05;    % pendulum mass [kg]
@@ -16,19 +16,24 @@ param.c   = 0.005;   % cart damping
 param.l   = 0.2;     % pendulum length [m]
 param.k_m = 1.0;     % motor gain
 
-disp('simulating ...')
+save_results = true;
+view_traj = true;
+out = simulate_system(h, Tsim, x0, u, param, save_results, view_traj);
 
-output = sim('simulated_system');
 
-disp('plotting ...')
-
-visualize_trajectory(output.tout, output.x, param)
-
-disp('saving data ...')
-
-timestamp = char(datetime('now', 'Format', 'yyyyMMdd_HHmmss'));
-filename = sprintf('sim_%s.mat', timestamp);
-save(fullfile(pwd, 'simulated_system', 'sim_results', filename), ...
-    'output', 'u', 'param')
-
-disp('DONE')
+% disp('simulating ...')
+% 
+% output = sim('simulated_system');
+% 
+% disp('plotting ...')
+% 
+% visualize_trajectory(output.tout, output.x, param)
+% 
+% disp('saving data ...')
+% 
+% timestamp = char(datetime('now', 'Format', 'yyyyMMdd_HHmmss'));
+% filename = sprintf('sim_%s.mat', timestamp);
+% save(fullfile(pwd, 'simulated_system', 'sim_results', filename), ...
+%     'output', 'u', 'param')
+% 
+% disp('DONE')
